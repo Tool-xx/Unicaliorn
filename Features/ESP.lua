16b71250ac6284332fb8bb61ce73eb2396e0a8ee
Tool-xx <xcumunin@tuta.io>
2026-07-11 00:50:52 +0300

    Update ESP.lua

--- Features/ESP.lua
+++ Features/ESP.lua
@@ -1,6 +1,7 @@
 -- Features/ESP.lua
 -- ESP Feature module
 -- Wraps Utils ESP functions into a clean Feature API
+-- All colors are dynamic black-white gradient (no manual color selection)
 -- Receives Context, returns Feature table
 
 return function(Context)
@@ -35,14 +36,6 @@ return function(Context)
         Utils.setBoxEnabled(false)
     end
 
-    function ESP.SetBoxColor(r, g, b)
-        Utils.setBoxColor(r, g, b)
-    end
-
-    function ESP.SetBoxColor3(color3)
-        Utils.setBoxColor(math.floor(color3.R * 255), math.floor(color3.G * 255), math.floor(color3.B * 255))
-    end
-
     function ESP.IsBoxEnabled()
         return FeatureState.espBoxEnabled
     end
@@ -56,14 +49,6 @@ return function(Context)
 
     function ESP.DisableHitbox()
         Utils.setHitboxEnabled(false)
-    end
-
-    function ESP.SetHitboxColor(r, g, b)
-        Utils.setHitboxColor(r, g, b)
-    end
-
-    function ESP.SetHitboxColor3(color3)
-        Utils.setHitboxColor(math.floor(color3.R * 255), math.floor(color3.G * 255), math.floor(color3.B * 255))
     end
 
     function ESP.IsHitboxEnabled()
@@ -81,33 +66,10 @@ return function(Context)
         Utils.setSkeletonEnabled(false)
     end
 
-    function ESP.SetSkeletonColor(r, g, b)
-        Utils.setSkeletonColor(r, g, b)
-    end
-
-    function ESP.SetSkeletonColor3(color3)
-        Utils.setSkeletonColor(math.floor(color3.R * 255), math.floor(color3.G * 255), math.floor(color3.B * 255))
-    end
-
     function ESP.IsSkeletonEnabled()
         return FeatureState.espSkeletonEnabled
     end
 
-    -- ============================================================
-    -- GET CURRENT COLORS
-    -- ============================================================
-    function ESP.GetBoxColor()
-        return FeatureState.espBoxColor
-    end
-
-    function ESP.GetHitboxColor()
-        return FeatureState.espHitboxColor
-    end
-
-    function ESP.GetSkeletonColor()
-        return FeatureState.espSkeletonColor
-    end
-
-    print("[Feature] ESP module loaded.")
+    print("[Feature] ESP module loaded (dynamic gradient).")
     return ESP
 end
